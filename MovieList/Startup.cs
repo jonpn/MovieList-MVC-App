e879a04e263ab2aaa4dc6d1a322fd957ee6400b0
Jonathon Nagassar <jonnagassar@live.com>
2021-02-18 12:04:07 -0500

    test

--- MovieList/Startup.cs
+++ MovieList/Startup.cs
@@ -36,7 +36,7 @@ namespace MovieList
                        Configuration.GetConnectionString("MovieContext"))
                );
         }
-
+        //test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
